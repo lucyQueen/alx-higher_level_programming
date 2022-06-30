517014fbaf3896967a37e76b126a9a2a65c4469a
lucyQueen <ayegbaluccia23@gmail.com>
2022-06-30 20:50:11 +0000

    program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+f __name__ == "__main__":
+    from sys import argv
+    temp = 0
+    numero2 = 0
+    for i in range(1, len(argv)):
+        numero = int(argv[i])
+        resultado = numero + numero2
+        numero2 = resultado
+        temp = resultado
+    print('{}'.format(temp))
